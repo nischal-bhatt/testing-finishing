1d5549539d2e1a89257e57cd322c1606450626a2
nischal-bhatt <nischal.bhatt.87@gmail.com>
2023-02-25 23:30:23 +0800

    gf

--- consolidated/UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/io/UsersRepositoryTest.java
+++ consolidated/UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/io/UsersRepositoryTest.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataJpaTest
@@ -33,5 +35,25 @@ public class UsersRepositoryTest {
         UserEntity e=usersRepository.findByEmail(u.getEmail());
 
         assertEquals(u.getFirstName(),e.getFirstName());
+    }
+
+    @Test
+    void NNNN()
+    {
+        UserEntity u = new UserEntity();
+        u.setFirstName("hhh");
+        u.setLastName("bbb");
+        u.setUserId("rrrr");
+        u.setEmail("a@a.com");
+        u.setEncryptedPassword("SSSS");
+
+        testEntityManager.persistAndFlush(u);
+
+       List<UserEntity> ls
+        =usersRepository.findUsersWithEmailEndingWith("@a.com");
+
+       assertEquals(u.getLastName(),ls.get(0).getLastName());
+
+
     }
 }
