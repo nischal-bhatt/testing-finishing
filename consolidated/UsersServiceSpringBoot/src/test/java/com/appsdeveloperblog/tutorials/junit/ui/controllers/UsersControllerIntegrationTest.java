cf359b4dae1fbaec4050979bf9e98a82c800d0aa
nischal-bhatt <nischal.bhatt.87@gmail.com>
2023-02-25 19:59:06 +0800

    oiio

---
+++ consolidated/UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
@@ -0,0 +1,72 @@
+package com.appsdeveloperblog.tutorials.junit.ui.controllers;
+
+import com.appsdeveloperblog.tutorials.junit.ui.response.UserRest;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.*;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.Arrays;
+
+
+@SpringBootTest
+        (webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT /*properties="server.port=8099"*/)
+@TestPropertySource(locations = "/application-test.properties")
+public class UsersControllerIntegrationTest {
+
+    @Value("${server.port}")
+    private int serverPort;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @LocalServerPort
+    private int localServerPort;
+
+    @Test
+    void contextLoads()
+    {
+        System.out.println(serverPort + " " + localServerPort);
+    }
+
+    @Test
+    @DisplayName("if user can be created")
+    void AAA() throws JSONException {
+        //arrange
+        JSONObject userDetails = new JSONObject();
+        userDetails.put("firstName","ser");
+        userDetails.put("lastName","hie");
+        userDetails.put("email","t@t.com");
+        userDetails.put("password","12345678");
+        userDetails.put("repeatPassword","12345678");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+        HttpEntity<String> request
+                = new HttpEntity<>(userDetails.toString(),headers);
+
+
+
+        //act
+        ResponseEntity<UserRest> createdobj
+        =testRestTemplate.postForEntity("/users",request,UserRest.class);
+
+        UserRest a = createdobj.getBody();
+
+
+
+
+        //assert
+        Assertions.assertEquals(HttpStatus.OK, createdobj.getStatusCode());
+    }
+}
